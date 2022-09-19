c1dd86efc7802209fcf003e6078b4e44ff0dfd20
gurudattpuranik25 <gurudattpuranik35@gmail.com>
2022-09-19 19:40:14 +0530

    added authentication

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,57 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { CommerceContext } from "./Context";
+import { auth, provider } from "../firebase-config";
+import { signInWithPopup } from "firebase/auth";
+import {
+  setActiveUser,
+  setUserLogoutState,
+  selectUserName,
+} from "../features/UserSlice";
 import "./Navbar.css";
 
 function Navbar() {
   const { searchItem, searchHandler } = useContext(CommerceContext);
 
   const cartCount = useSelector((state) => state.cart.cartArray.length);
+
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          setActiveUser({
+            userName: authUser.displayName,
+            userEmail: authUser.email,
+          })
+        );
+      } else {
+        dispatch(setUserLogoutState());
+      }
+    });
+  }, []);
+
+  const handleLogin = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      dispatch(
+        setActiveUser({
+          userName: result.user.displayName,
+          userEmail: result.user.email,
+        })
+      );
+    });
+  };
+  const handleLogout = () => {
+    auth
+      .signOut()
+      .then(() => {
+        dispatch(setUserLogoutState());
+      })
+      .catch((err) => alert(err.message));
+  };
 
   return (
     <div className=" navbar">
@@ -24,6 +68,14 @@ function Navbar() {
       <div className="nav__links">
         <Link to="/">Home</Link>
         <Link to="/products">Products</Link>
+        {userName ? (
+          <div className=" flex gap-5">
+            <p>Hi, {userName.toUpperCase()}</p>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        ) : (
+          <button onClick={handleLogin}>Login</button>
+        )}
         <Link to="/cart" className=" cart__icon">
           <i className="fa-solid fa-cart-shopping"></i>
           <span className=" cart__count w-[1.5rem] h-[1.5rem] bg-rose-500 text-white flex justify-center items-center rounded-full ">
--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,19 +1,75 @@
 import React, { useContext } from "react";
-import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+import { addToCart } from "../features/cartSlice";
 import { CommerceContext } from "./Context";
+import "./ProductPage.css";
 
 function ProductPage() {
   const { productId } = useParams();
   const { products } = useContext(CommerceContext);
   const selectedItem = products.find((item) => item.id === productId);
 
+  const dispatch = useDispatch();
+
   return (
     <div>
       {selectedItem && (
-        <div>
-          <img src={selectedItem.imageUrl} alt="" />
-          <h1>{selectedItem.title}</h1>
-          <p>{selectedItem.description}</p>
+        <div className="product__container">
+          <div className="selected__product">
+            <img src={selectedItem.imageUrl} alt="" />
+            <div className="details">
+              <h1>{selectedItem.title}</h1>
+              <hr />
+              <p>{selectedItem.description}</p>
+              <p>⭐{selectedItem.rating}</p>
+
+              <p>$ {selectedItem.price}</p>
+              <p className="text-gray-400 ">Inclusive of all taxes</p>
+              <div className=" flex mt-4 gap-6">
+                <div className=" flex flex-col items-center gap-1">
+                  <i className="fa-solid fa-arrow-rotate-left"></i>
+                  <span className=" text-center text-blue-700 font-semibold">
+                    10 days <br /> returnable
+                  </span>
+                </div>
+                <div className=" flex flex-col items-center gap-1">
+                  <i className="fa-solid fa-truck"></i>
+                  <span className=" text-center text-blue-700 font-semibold">
+                    Unveiled
+                    <br /> Delivered
+                  </span>
+                </div>
+                <div className=" flex flex-col items-center gap-1">
+                  <i className="fa-solid fa-building-circle-check"></i>
+                  <span className=" text-center text-blue-700 font-semibold">
+                    No-Contact <br /> Delivery
+                  </span>
+                </div>
+              </div>
+              <div className="btn">
+                <button
+                  className="cart__btn"
+                  onClick={() =>
+                    dispatch(
+                      addToCart({
+                        id: selectedItem.id,
+                        imageUrl: selectedItem.imageUrl,
+                        title: selectedItem.title,
+                        price: selectedItem.price,
+                        quantity: 1,
+                      })
+                    )
+                  }
+                >
+                  Add to cart
+                </button>
+                <button className="home__btn">
+                  <Link to="/products">Home</Link>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </div>
