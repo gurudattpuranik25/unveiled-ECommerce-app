8fa7abc35171cb1970f35bdbf66366b757b373e9
gurudattpuranik25 <gurudattpuranik35@gmail.com>
2022-11-25 11:58:31 +0530

    modified error handling

--- src/components/Address.js
+++ src/components/Address.js
@@ -3,6 +3,8 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import "./Address.css";
 
+const validator = require("validator");
+
 function Address() {
   const [formData, setFormData] = useState({
     name: "",
@@ -26,17 +28,29 @@ function Address() {
   };
 
   const handleOrders = (formData) => {
-    if (
-      formData.name === "" ||
-      formData.email === "" ||
-      formData.phoneNumber === "" ||
-      formData.address === "" ||
-      formData.pincode === "" ||
-      formData.cardNumber === "" ||
-      formData.cvv === ""
-    ) {
-      alert("Please enter correct address/payment details");
-    } else {
+    if (formData.name === "") alert("Please provide your name.");
+    else if (!validator.isEmail(formData.email))
+      alert("Please provide the correct email address.");
+    else if (formData.phoneNumber.length !== 10)
+      alert("Please provide a 10 digit phone number");
+    else if (formData.address === "") alert("Please provide the address");
+    else if (formData.pincode.length !== 6)
+      alert("Please provide a 6 digit pincode");
+    else if (formData.cardNumber.length !== 12)
+      alert("Please provide your 12 digit card number");
+    else if (formData.cvv.length !== 3)
+      alert("Please provide the correct cvv number");
+    // if (
+    //   formData.name === "" ||
+    //   formData.email === "" ||
+    //   formData.phoneNumber === "" ||
+    //   formData.address === "" ||
+    //   formData.pincode === "" ||
+    //   formData.cardNumber === "" ||
+    //   formData.cvv === ""
+    // ) {
+    // alert("Please enter correct address/payment details");}
+    else {
       setSummary(
         `Payment successful 😇.Your shipment will be delivered to ${formData.name}, ${formData.address} - ${formData.pincode}. `
       );
